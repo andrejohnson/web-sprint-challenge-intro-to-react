76c430bcf6219220b9ded54ae2964d6d595e1786
Andre_Johnson <Andrejohnson513@gmail.com>
2021-10-08 16:37:44 -0400

    done

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CharacterStyle = styled.div `
+const CharStyle = styled.div `
     
     border: 3px solid blue;
     display: flex;
@@ -30,13 +30,20 @@ const CharacterStyle = styled.div `
 export default function Character({info}) {
   const [visible, setVisible] = React.useState(false);
     return (
-        <CharacterStyle >
+        <CharStyle >
         <button onClick={() => setVisible(!visible)}>
         {visible ? 'Hide' : 'Show'}
        </button>
        <h2>{info.name}</h2>
       {visible && <div className="style">   
-        <iframe className="fram" width="200" height="120" src={info.i} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className="fram" width="200"
+         height="120" src={info.i} 
+         title="YouTube video player" 
+         frameborder="0" 
+        allow="accelerometer; autoplay; 
+        clipboard-write; encrypted-media;
+        gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
             <h3>Gender: {info.gender}</h3>
             <h3>Height: {info.height}</h3>
             <h3>Mass: {info.mass}</h3>
@@ -45,7 +52,7 @@ export default function Character({info}) {
             <h3>Hair Color: {info.hair_color}</h3>
             <h3>Skin Color: {info.skin_color}</h3>
             </div>}
-        </CharacterStyle>
+        </CharStyle>
     )
 }
 
