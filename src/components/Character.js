67ec220a0f13b2c7a4db44c033a528f3561e49f4
Andre_Johnson <Andrejohnson513@gmail.com>
2021-10-08 10:46:37 -0400

    secound

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react"
+import reactDom from "react-dom"
+import axios from "axios"
+import { rest } from "msw";
+import { handlers, data } from "../mocks/handlers";
+
+
+
+
+const Character  = () => {
+
+  const [strName, setSrtName] = useState('')
+
+    useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(rp =>{
+      console.log(rp.data.films, 'start');
+      setSrtName(rp.data.films, console.log('mid'))
+      console.log(rp.data.films, 'end');
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  
+  }, [])
+ 
+return(
+  <div className ='name'>
+  <h1>{strName}</h1>
+</div>
+)
+
+
+}
+
+export default Character;
